83ed0a2e7c0ecd6e13298e10fd026df7838ad1b6
shota suzuki <suzusho956@yahoo.co.jp>
2025-04-20 15:25:04 +0900

    問題２Q1 回答追加

--- PHP_practice①/php-practice/php-practice_2.php
+++ PHP_practice①/php-practice/php-practice_2.php
@@ -1,6 +1,18 @@
 <?php
 // Q1 tic-tac問題
+echo "1から100までのカウントを開始します\n\n";
 
+for ($i = 1; $i <= 100; $i++) {
+  if ($i % 4 === 0 && $i % 5 === 0) {
+    echo "tic-tac\n";
+  } elseif ($i % 4 === 0) {
+    echo "tic\n";
+  } elseif ($i % 5 === 0) {
+    echo "tac\n";
+  } else {
+    echo "$i\n";
+  }
+}
 
 // Q2 多次元連想配列
 
@@ -12,4 +24,3 @@
 
 
 // Q5 定義済みクラス
-?>
